c9f5722f5123dc7b973fbf965258c1469ffa1d19
zzFreeY <516768101@qq.com>
2022-01-24 23:55:04 +0800

    User

---
+++ cloud-users/src/main/java/zzfree/service/UserService.java
@@ -0,0 +1,17 @@
+package zzfree.service;
+
+import zzfree.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    /**
+     * 根据条件分页查询
+     */
+    List<User> findAllByKeywords(int offset, int limit, String id, String name, String phone);
+
+    /**
+     * 根据条件查询总条数
+     */
+    Long findTotalCountsByKeywords(String id, String name, String phone);
+}
